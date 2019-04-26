43f67d04c3b9bd8d37bb92b1605b11d014d6a07f
Tho Trinh <tho.q.trinh-1@ou.edu>
2019-04-26 12:53:52 -0500

    Update FRAMES

--- HDFrame.java
+++ HDFrame.java
@@ -1,5 +1,20 @@
+import java.awt.Dimension;
 
-public class HDFrame
+import javax.swing.JFrame;
+
+public class HDFrame extends JFrame
 {
+    public HDFrame() {
+    setSize(new Dimension(500, 500));
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    pack();
+    setVisible(true);
+    }
+    
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        new HDFrame();
+    }
 
 }
