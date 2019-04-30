a85d06217711cf21ec5c0459919112947d1259d2
Tho Trinh <tho.q.trinh-1@ou.edu>
2019-04-30 12:16:55 -0500

    Deleted unneeded methods in MesoAscii

--- MesoAscii.java
+++ MesoAscii.java
@@ -17,31 +17,4 @@ public class MesoAscii {
         wordChars = STID.toCharArray();
     }
     
-    /*
-     * (non-Javadoc)
-     * @see MesoAsciiAbstract#calAverage()
-     * 
-     * Calculates average of station ID
-     */
-    public int calAverage()
-    {
-        double sum = 0;
-        
-        for(char letter: wordChars) {
-            sum += getAscii(letter);
-        }
-        
-        if((sum / (double) wordChars.length) % 1 >= .50) {
-            return (int) Math.ceil(sum / wordChars.length); //Assigns Ascii Average
-        }else {
-            return (int) Math.floor(sum / wordChars.length);//Assigns Ascii Average
-        }
-    }
-    
-  //Gets the Ascii Value of an individual letter
-    public int getAscii(char letter) {
-        
-        return (int)letter;
-    }
-    
 }
